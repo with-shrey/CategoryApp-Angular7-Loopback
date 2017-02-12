cdfb70fa11c95ca5833eee246e6957df19ee5caa
Aquid Shahwar <aquid.shahwar@gmail.com>
2017-02-13 03:17:49 +0530

    Fixed issue for “url not found”
    Earlier api rest methods were not working, I updated the dependencies with stable versions and now things are working.
    FindOrCreate is having some issues and have added a workaround for that as suggested in https://github.com/strongloop/loopback-component-passport/issues/123

--- server/boot/seed-user.js
+++ server/boot/seed-user.js
@@ -1,40 +1,42 @@
 var Promise = require('bluebird');
 module.exports = function (app, cb) {
-	var user = app.models.User;
-	Promise.resolve()
-	.then(function(){
-		return user.findOrCreate(
+  var user = app.models.User;
+  Promise.resolve()
+    .then(function () {
+      return user.findOrCreate(
         {
-            where: {
-                email: 'goodjob@shoppinpal.com'
-            }
-        }, 
-        {
-            username: 'goodjob',
-            email: 'goodjob@shoppinpal.com',
-            password: 'test123'
-        });
-	})
-	.then(function(user_one){
-		console.log('User 1: ', user_one);
-		return user.findOrCreate(
-        {
-        	where: {
-                email: 'yourock@shoppinpal.com'
-            }
+          where: {
+            email: 'goodjob@shoppinpal.com'
+          }
         },
         {
-            username: 'you_rock',
-            email: 'yourock@shoppinpal.com',
-            password: 'test123'
-        });
-	})
-	.then(function(user_two){
-    	console.log('User 2: ',user_two);
-        return cb();
+          username: 'goodjob',
+          email: 'goodjob@shoppinpal.com',
+          password: 'test123'
+        },
+        {validate: false});
+    })
+    .then(function (user_one) {
+      console.log('User 1: ', user_one);
+      return user.findOrCreate(
+        {
+          where: {
+            email: 'yourock@shoppinpal.com'
+          }
+        },
+        {
+          username: 'you_rock',
+          email: 'yourock@shoppinpal.com',
+          password: 'test123'
+        },
+        {validate: false});
+    })
+    .then(function (user_two) {
+      console.log('User 2: ', user_two);
+      return cb();
     })
     .catch(function (error) {
-        console.log('error in creating ', error);
-        return cb(error);
+      console.log('error in creating ', error);
+      return cb(error);
     });
 };
