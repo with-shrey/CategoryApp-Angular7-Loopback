7a7d0b20d3ddc294b522a0dd832c27df6626e693
Shrey Gupta <connectwithshrey@gmail.com>
2019-03-07 00:54:59 +0530

    Loader

--- client/dist/client/common.js
+++ client/dist/client/common.js
@@ -23,65 +23,6 @@ var User = /** @class */ (function () {
 
 
 
-/***/ }),
-
-/***/ "./src/app/modules/auth/shared/service/auth.service.ts":
-/*!*************************************************************!*\
-  !*** ./src/app/modules/auth/shared/service/auth.service.ts ***!
-  \*************************************************************/
-/*! exports provided: AuthService */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthService", function() { return AuthService; });
-/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
-/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common/http */ "./node_modules/@angular/common/fesm5/http.js");
-/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
-
-
-
-
-var AuthService = /** @class */ (function () {
-    function AuthService(http, router) {
-        this.http = http;
-        this.router = router;
-        this._users = 'Users';
-        this._login_user = this._users + '/login';
-    }
-    AuthService.prototype.registerUser = function (user) {
-        var id = user.id, confirm_password = user.confirm_password, restUser = tslib__WEBPACK_IMPORTED_MODULE_0__["__rest"](user, ["id", "confirm_password"]);
-        return this.http.post(this._users, restUser);
-    };
-    AuthService.prototype.loginUser = function (user) {
-        var email = user.email, password = user.password, restUser = tslib__WEBPACK_IMPORTED_MODULE_0__["__rest"](user, ["email", "password"]);
-        return this.http.post(this._login_user, { email: email, password: password });
-    };
-    AuthService.prototype.getUserId = function () {
-        return localStorage.getItem('userId');
-    };
-    AuthService.prototype.getToken = function () {
-        return localStorage.getItem('token');
-    };
-    AuthService.prototype.getUser = function () {
-        return this.http.get(this._users + '/' + this.getUserId() + '?access_token=' + this.getToken());
-    };
-    AuthService.prototype.logout = function () {
-        localStorage.clear();
-        this.router.navigate(['auth/login']);
-    };
-    AuthService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
-        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
-            providedIn: 'root'
-        }),
-        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"], _angular_router__WEBPACK_IMPORTED_MODULE_3__["Router"]])
-    ], AuthService);
-    return AuthService;
-}());
-
-
-
 /***/ })
 
 }]);
--- client/src/app/shared/services/interceptor.service.ts
+++ client/src/app/shared/services/interceptor.service.ts
@@ -4,11 +4,13 @@ import {ToastrService} from 'ngx-toastr';
 import {Observable, throwError} from 'rxjs';
 import {catchError, finalize, map} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
+import {AuthService} from '../../modules/auth/shared/service/auth.service';
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
   constructor(
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private auth: AuthService
   ) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     req = req.clone({
@@ -27,8 +29,12 @@ export class InterceptorService implements HttpInterceptor {
       catchError(error => {
         if (error.error.error) {
           this.toastr.error(error.error.error.message.split(';').join('\n'), error.error.error.name);
+          if (error.error.error.status === 401) {
+            this.auth.logout();
+          }
         } else {
           this.toastr.error(error.message, 'Error');
+
         }
         return throwError(error);
       })
